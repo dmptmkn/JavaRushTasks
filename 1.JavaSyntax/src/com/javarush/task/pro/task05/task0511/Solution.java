260b669449a5a7a35d1fb834eb47e332e2e2187c
dmptmkn <dm.ptmkn@yandex.ru>
2023-10-30 13:18:05 +0300

    add solution to task 0511

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0511/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0511/Solution.java
@@ -10,6 +10,14 @@ public class Solution {
     public static int[][] multiArray;
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Scanner scan = new Scanner(System.in);
+        int columns = scan.nextInt();
+        multiArray = new int[columns][];
+        int[] rows = new int[columns];
+
+        for (int i = 0; i < rows.length; i++) {
+            rows[i] = scan.nextInt();
+            multiArray[i] = new int[rows[i]];
+        }
     }
 }
