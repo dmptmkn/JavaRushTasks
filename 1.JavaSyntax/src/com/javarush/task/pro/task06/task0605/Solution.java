39a5104aa86ae5879354e27650ef99b38ccf4927
dmptmkn <dm.ptmkn@yandex.ru>
2023-10-30 22:59:20 +0300

    add solutions to tasks 0605, 0606

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0605/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.pro.task06.task0605;
+
+/* 
+Правильный порядок
+*/
+
+import javax.sound.midi.Soundbank;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] array = {11, 22, 33, 44, 55, 66, 77, 88, 99};
+        printArray(array);
+        reverseArray(array);
+        printArray(array);
+    }
+
+    public static void reverseArray(int[] array) {
+        for (int i = 0, j = array.length - 1; i <= j; i++, j--) {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,53 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod() {
+
+    }
+
+    public static void universalMethod(int x) {
+
+    }
+
+    public static void universalMethod(int x, String y) {
+
+    }
+
+    public static void universalMethod(char a) {
+
+    }
+
+    public static void universalMethod(boolean isOk) {
+
+    }
+
+    public static void universalMethod(double x) {
+
+    }
+
+    public static void universalMethod(char a, double l) {
+
+    }
+
+    public static void universalMethod(String s, boolean a, int x) {
+
+    }
+
+    public static void universalMethod(int[] a, String s, char b) {
+
+    }
+
+    public static void universalMethod(String k, int a, double e) {
+
+    }
+
+}
