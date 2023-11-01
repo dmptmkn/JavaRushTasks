d1a21cc2065586e8d7cb1832e4497c3c37a59c78
dmptmkn <dm.ptmkn@yandex.ru>
2023-11-01 19:47:53 +0300

    add solutions to tasks 0712, 0713

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String strOne = new String("Hi");
+        String strTwo = new String("there");
+        String strThree = new String("!");
+        String strFour = new String("It's");
+        String strFive = new String("time");
+        String strSix = new String("to");
+        String strSeven = new String("wake");
+        String strEight = new String("the");
+        String strNine = new String("hell");
+        String strTen = new String("up");
+        int[] intOne = new int[1];
+        int[] intTwo = new int[2];
+        int[] intThree = new int[3];
+        int[] intFour = new int[4];
+        int[] intFive = new int[5];
+        Scanner scanOne = new Scanner(System.in);
+        Scanner scanTwo = new Scanner(System.in);
+    }
+}
