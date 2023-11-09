bde9bb7eb8281a65f3334a0e4d12fa93e9ee7db8
dmptmkn <dm.ptmkn@yandex.ru>
2023-11-09 17:18:06 +0300

    add solution to task 1004

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1004/CarConcern.java
@@ -0,0 +1,30 @@
+package com.javarush.task.pro.task10.task1004;
+
+/* 
+Жажда скорости
+*/
+
+public class CarConcern {
+    private final String manufacturer = "Lamborghini";
+    private final String model;
+    private final int year;
+    private final String color;
+
+    public CarConcern(String model, int year, String color) {
+        this.year = year;
+        this.color = color;
+        this.model = model;
+    }
+
+    public CarConcern(String model, int year) {
+        this.year = year;
+        this.color = "Оранжевый";
+        this.model = model;
+    }
+
+    public CarConcern(String model) {
+        this.year = 4321;
+        this.color = "Оранжевый";
+        this.model = model;
+    }
+}
