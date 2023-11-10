68215580c6a65f52321c673c7da6deb685301770
dmptmkn <dm.ptmkn@yandex.ru>
2023-11-10 22:32:21 +0300

    add solution to task 1017

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1017/Earth.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task10.task1017;
+
+/* 
+Создание материков
+*/
+
+public class Earth {
+    public static void main(String[] args) {
+        Antarctica antarctica = new Antarctica(13);
+        Australia australia = new Australia(7);
+        Eurasia eurasia = new Eurasia(52);
+        NorthAmerica northAmerica = new NorthAmerica(24);
+        SouthAmerica southAmerica = new SouthAmerica(17);
+        Africa africa = new Africa(30);
+    }
+}
