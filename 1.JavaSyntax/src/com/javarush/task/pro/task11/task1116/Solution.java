65c11b371cab9af6874a5ff9648ef4412ef734bf
dmptmkn <dm.ptmkn@yandex.ru>
2023-11-13 18:33:51 +0300

    add solution to task 1116

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1116/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task11.task1116;
+
+/* 
+Правильный класс
+*/
+
+class solution {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        System.out.println(cat.name);
+    }
+}
