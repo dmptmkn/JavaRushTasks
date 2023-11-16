85179cf3ac3a61d99b327940e7b576d77613886a
dmptmkn <dm.ptmkn@yandex.ru>
2023-11-16 16:44:59 +0300

    add solution to task 1316

--- 1.JavaSyntax/src/com/javarush/task/pro/task13/task1316/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1316/Solution.java
@@ -3,6 +3,9 @@ package com.javarush.task.pro.task13.task1316;
 public class Solution {
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        JavarushQuest[] javarushQuests = JavarushQuest.values();
+        for (int i = 0; i < javarushQuests.length; i++) {
+            System.out.println(javarushQuests[i].ordinal());
+        }
     }
 }
