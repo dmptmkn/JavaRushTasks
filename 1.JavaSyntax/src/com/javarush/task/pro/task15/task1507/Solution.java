4ed62d83ab1876a7f17ef7ffd172f18698a4479e
dmptmkn <dm.ptmkn@yandex.ru>
2023-11-27 14:55:19 +0300

    add solution to task 1507

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1507/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task15.task1507;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+/* 
+Пропускаем не всех
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        Path path = Paths.get(scan.nextLine());
+        List<String> list = Files.readAllLines(path);
+
+        for (int i = 0; i < list.size(); i++) {
+            if (i % 2 == 0) {
+                System.out.println(list.get(i));
+            }
+        }
+    }
+}
+
