fc1c052bd59aca86db82c6c97dd77ca7b77f38db
dmptmkn <dm.ptmkn@yandex.ru>
2023-12-13 15:01:49 +0300

    add solution to task core1207

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    print(25);
+    print(new Integer("28"));
+    }
+
+    public static void print(int n) {
+
+    }
+
+    public static void print(Integer n) {
+
+    }
+}
