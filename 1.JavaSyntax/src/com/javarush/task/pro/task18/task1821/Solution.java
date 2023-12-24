1e6b6d8c8cee2fec2db87d44dd4a296d983321af
dmptmkn <dm.ptmkn@yandex.ru>
2023-12-24 22:01:58 +0300

    add new solution to task 1821

--- 1.JavaSyntax/src/com/javarush/task/pro/task18/task1821/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1821/Solution.java
@@ -37,7 +37,7 @@ public class Solution {
     }
 
     public static Optional<Car> getCheaperCar(Stream<Car> cars, Car cheapestCar) {
-        return cars.findFirst().filter(car -> car.getPrice() < cheapestCar.getPrice());
+        return cars.filter(car -> car.getPrice() < cheapestCar.getPrice()).findFirst();
     }
 }
 
