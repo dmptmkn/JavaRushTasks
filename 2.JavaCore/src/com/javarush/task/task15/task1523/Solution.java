7eed8afe5cbb34b04fab7963f9b95a87b4024cd1
dmptmkn <dm.ptmkn@yandex.ru>
2023-12-24 19:35:54 +0300

    add solution to task core1523

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+
+    public Solution() {
+    }
+
+    private Solution(int a) {
+    }
+
+    Solution(String s) {
+    }
+
+    protected Solution(int a, int b) {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+    public SubSolution() {
+    }
+
+    SubSolution(String s) {
+    }
+
+    protected SubSolution(int a) {
+    }
+
+}
