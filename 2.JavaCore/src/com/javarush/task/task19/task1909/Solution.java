79f5101289b852011f97fc06c25122ad08d409cf
dmptmkn <dm.ptmkn@yandex.ru>
2024-01-25 13:46:52 +0300

    add solution to task core1909

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1909/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task19.task1909;
+
+import java.io.*;
+import java.util.ArrayList;
+
+/* 
+Замена знаков
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader reader = new BufferedReader(new FileReader(console.readLine()));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(console.readLine()))) {
+            while (reader.ready()) {
+                String nextLine = reader.readLine().replace(".", "!");
+                writer.write(nextLine);
+            }
+        } catch (Exception e) {
+        }
+    }
+}
