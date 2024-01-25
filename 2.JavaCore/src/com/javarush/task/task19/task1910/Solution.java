7debcd9c8ca2fbefff21c9855d1355223e6e2749
dmptmkn <dm.ptmkn@yandex.ru>
2024-01-25 14:15:01 +0300

    add solution to task core1910

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1910/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task19.task1910;
+
+import java.io.*;
+import java.util.ArrayList;
+
+/* 
+Пунктуация
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader reader = new BufferedReader(new FileReader(console.readLine()));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(console.readLine()))) {
+            while (reader.ready()) {
+                String nextLine = reader.readLine().replaceAll("\\p{P}", "").replace("\n", "");
+                writer.write(nextLine);
+            }
+        } catch (Exception e) {
+        }
+    }
+}
