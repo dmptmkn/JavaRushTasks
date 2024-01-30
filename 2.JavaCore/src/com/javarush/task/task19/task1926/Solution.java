613f62f0690e7811e7a09ce60ec5e37e34114c0d
dmptmkn <dm.ptmkn@yandex.ru>
2024-01-30 11:41:44 +0300

    add solution to task core1926

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1926/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task19.task1926;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Перевертыши
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader reader = new BufferedReader(new FileReader(console.readLine()))) {
+            while (reader.ready()) {
+                String line = reader.readLine();
+                StringBuilder builder = new StringBuilder(line);
+                builder.reverse();
+                System.out.println(builder);
+            }
+        } catch (Exception e) {
+        }
+    }
+}
