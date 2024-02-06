d19948d49ccc48ce32ff573ef364cab1667d0075
dmptmkn <dm.ptmkn@yandex.ru>
2024-02-06 14:40:49 +0300

    add solution to task core2017

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2017/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task20.task2017;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+
+/* 
+Десериализация
+*/
+
+public class Solution {
+    public A getOriginalObject(ObjectInputStream objectStream) throws IOException {
+        A a = new A();
+        try {
+            if (objectStream.readObject() instanceof A) {
+                a = (A) objectStream.readObject();
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return a;
+    }
+
+    public class A implements Serializable {
+    }
+
+    public class B extends A {
+        public B() {
+            System.out.println("inside B");
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
