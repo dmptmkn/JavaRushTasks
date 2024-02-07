4fce9ebfa1493cfe384856d66ce3bf9e8745d7c7
dmptmkn <dm.ptmkn@yandex.ru>
2024-02-07 22:47:02 +0300

    add solution to task multithreading2109

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2109/Solution.java
@@ -0,0 +1,107 @@
+package com.javarush.task.task21.task2109;
+
+/* 
+Запретить клонирование
+*/
+
+import java.util.Objects;
+
+public class Solution {
+    public static class A implements Cloneable {
+        private int i;
+        private int j;
+
+        public A(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+
+        public int getI() {
+            return i;
+        }
+
+        public int getJ() {
+            return j;
+        }
+
+        @Override
+        public A clone() throws CloneNotSupportedException {
+            try {
+                A clone = (A) super.clone();
+                // TODO: copy mutable state here, so the clone can't change the internals of the original
+                return clone;
+            } catch (CloneNotSupportedException e) {
+                throw new AssertionError();
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            A a = (A) o;
+            return i == a.i && j == a.j;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(i, j);
+        }
+    }
+
+    public static class B extends A {
+        private String name;
+
+        public B(int i, int j, String name) {
+            super(i, j);
+            this.name = name;
+        }
+
+        public B superClone() throws CloneNotSupportedException {
+            B clone = (B) super.clone();
+            return clone;
+        }
+
+        @Override
+        public B clone() throws CloneNotSupportedException {
+            throw new CloneNotSupportedException();
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            if (!super.equals(o)) return false;
+            B b = (B) o;
+            return Objects.equals(name, b.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(super.hashCode(), name);
+        }
+    }
+
+    public static class C extends B implements Cloneable {
+        public C(int i, int j, String name) {
+            super(i, j, name);
+        }
+
+        @Override
+        public C clone() {
+            try {
+                return (C) super.superClone();
+            } catch (CloneNotSupportedException e) {
+                throw new AssertionError();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
