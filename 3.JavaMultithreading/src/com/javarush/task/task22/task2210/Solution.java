437166ca23fe90c8e7a3bc8573e89d1c19a92748
dmptmkn <dm.ptmkn@yandex.ru>
2024-02-09 22:58:47 +0300

    add solution to task multithreading2210

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2210/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task22.task2210;
+
+import java.util.StringTokenizer;
+
+/* 
+StringTokenizer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public static String[] getTokens(String query, String delimiter) {
+        StringTokenizer tokenizer = new StringTokenizer(query, delimiter);
+        String[] result = new String[tokenizer.countTokens()];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = tokenizer.nextToken();
+        }
+
+        return result;
+    }
+}
