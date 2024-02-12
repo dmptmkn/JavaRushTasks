b05750694e1e585bb5296f7650c130276710809b
dmptmkn <dm.ptmkn@yandex.ru>
2024-02-12 13:29:16 +0300

    add solution to task multithreading2312.3

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Mouse.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Mouse.java
@@ -2,7 +2,27 @@ package com.javarush.task.task23.task2312;
 
 public class Mouse {
 
+    public Mouse(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     private int x;
     private int y;
 
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
