24b04a600e7292bbe979e133398983def66dca34
dmptmkn <dm.ptmkn@yandex.ru>
2024-02-15 10:09:21 +0300

    add solution to task core1807

--- 2.JavaCore/src/com/javarush/task/task18/task1807/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1807/Solution.java
@@ -1,25 +1,27 @@
-package com.javarush.task.task18.task1802;
+package com.javarush.task.task18.task1807;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 /* 
-Минимальный байт
+Подсчет запятых
 */
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         FileInputStream inputStream = new FileInputStream(new BufferedReader(new InputStreamReader(System.in)).readLine());
-        int minByte = Integer.MAX_VALUE;
+        int commaCount = 0;
 
         while (inputStream.available() > 0) {
             int nextByte = inputStream.read();
-            if (nextByte < minByte) {
-                minByte = nextByte;
+            if (nextByte == 44) {
+                commaCount++;
             }
         }
         inputStream.close();
-        System.out.println(minByte);
+
+        System.out.println(commaCount);
     }
 }
