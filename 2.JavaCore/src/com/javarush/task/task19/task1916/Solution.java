55cab274723b5089d3eab9bc29e0a9e828d1d5da
dmptmkn <dm.ptmkn@yandex.ru>
2024-02-18 18:58:03 +0300

    add updated solution to task core1916

--- 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task19.task1916;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,22 +14,36 @@ public class Solution {
     public static List<LineItem> lines = new ArrayList<LineItem>();
 
     public static void main(String[] args) {
+        List<String> file1 = new ArrayList<>();
+        List<String> file2 = new ArrayList<>();
         try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
              BufferedReader reader1 = new BufferedReader(new FileReader(console.readLine()));
              BufferedReader reader2 = new BufferedReader(new FileReader(console.readLine()))) {
             while (reader1.ready()) {
-                String nextLine1 = reader1.readLine();
-                String nextLine2 = reader2.readLine();
-                if (nextLine1.equals(nextLine2)) {
-                    lines.add(new LineItem(Type.SAME, nextLine1));
-                } else if (nextLine1.equals("")) {
-                    lines.add(new LineItem(Type.ADDED, nextLine2));
-                } else if (nextLine2.equals("")) {
-                    lines.add(new LineItem(Type.REMOVED, nextLine1));
-                }
+                file1.add(reader1.readLine());
+            }
+            while (reader2.ready()) {
+                file2.add(reader2.readLine());
             }
         } catch (Exception e) {
         }
+
+        while (file1.size() > 0 && file2.size() > 0) {
+            if (file1.get(0).equals(file2.get(0))) {
+                lines.add(new LineItem(Type.SAME, file1.get(0)));
+                file1.remove(0);
+                file2.remove(0);
+            } else if (file1.get(1).equals(file2.get(0))) {
+                lines.add(new LineItem(Type.REMOVED, file1.get(0)));
+                file1.remove(0);
+            } else {
+                lines.add(new LineItem(Type.ADDED, file2.get(0)));
+                file2.remove(0);
+            }
+        }
+
+        if (!(file1.isEmpty())) file1.stream().forEach(s -> lines.add(new LineItem(Type.REMOVED, s)));
+        if (!(file2.isEmpty())) file2.stream().forEach(s -> lines.add(new LineItem(Type.ADDED, s)));
     }
 
 
