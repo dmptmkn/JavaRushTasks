e7aa779c7bc36d000df9a0daf2c9eba714ea6634
dmptmkn <dm.ptmkn@yandex.ru>
2024-02-21 16:01:44 +0300

    add solution to task multithreading2413.10

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -12,6 +12,24 @@ public class Canvas {
         this.matrix = new char[height + 2][width + 2];
     }
 
+    void setPoint(double x, double y, char c) {
+        if (x < 0 || y < 0 || y >= matrix.length || x >= matrix[0].length) return;
+        int xIndex = (int) Math.round(x);
+        int yIndex = (int) Math.round(y);
+        matrix[yIndex][xIndex] = c;
+    }
+
+    void drawMatrix(double x, double y, int[][] matrix, char c) {
+        int xIndex = (int) Math.round(x);
+        int yIndex = (int) Math.round(y);
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (!(matrix[i][j] == 0)) setPoint(xIndex + j, yIndex + i, c);
+            }
+        }
+    }
+
     public int getWidth() {
         return width;
     }
