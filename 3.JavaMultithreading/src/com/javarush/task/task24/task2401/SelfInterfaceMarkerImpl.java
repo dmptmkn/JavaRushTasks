c937cd284e361130adee0ae23a1b83d64f73e237
dmptmkn <dm.ptmkn@yandex.ru>
2024-02-24 20:17:56 +0300

    add solution to task multithreading2401

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+
+    public void move() {
+        System.out.println("Moving...");
+    }
+
+    public void sit() {
+        System.out.println("Sitting...");
+    }
+}
