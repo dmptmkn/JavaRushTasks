0a89665a6cb7a531fce6aa5ca6656df06dbd9844
dmptmkn <dm.ptmkn@yandex.ru>
2024-02-27 14:29:12 +0300

    add solution to task multithreading2909.8

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Human implements Alive {
-    public static int nextId = 0;
+    private static int nextId = 0;
     private int id;
     protected int age;
     private List<Human> children = new ArrayList<>();
@@ -77,11 +77,6 @@ public class Human implements Alive {
     public int getId() {
         return id;
     }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public void printSize() {
         System.out.println("Рост: " + size[0] + " Вес: " + size[1]);
     }
