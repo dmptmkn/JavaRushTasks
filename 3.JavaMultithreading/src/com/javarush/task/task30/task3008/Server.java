3ff426819cd4396bb4d4b6a738df7425f58e2462
dmptmkn <dm.ptmkn@yandex.ru>
2024-03-13 15:10:44 +0300

    add solution to task multithreading3008.7

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,8 +3,22 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    public static void sendBroadcastMessage(Message message) {
+        try {
+            for (Map.Entry<String, Connection> entry : connectionMap.entrySet()) {
+                entry.getValue().send(message);
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка при отправке сообщения");
+        }
+    }
 
     public static void main(String[] args) {
         ConsoleHelper.writeMessage("Введите порт сервера");
@@ -24,6 +38,7 @@ public class Server {
     private static class Handler extends Thread {
 
         Socket socket;
+
         public Handler(Socket socket) {
             this.socket = socket;
         }
