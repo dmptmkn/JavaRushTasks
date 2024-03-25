3116edcc7a4c2210ad7be8a8f629c3dfe0db60fc
dmptmkn <dm.ptmkn@yandex.ru>
2024-03-25 11:17:07 +0300

    add solution to task multithreading3008.18

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task30.task3008.client;
+
+public class BotClient extends Client {
+
+    public static void main(String[] args) {
+        BotClient botClient = new BotClient();
+        botClient.run();
+    }
+
+    @Override
+    protected SocketThread getSocketThread() {
+        return new BotSocketThread();
+    }
+
+    @Override
+    protected boolean shouldSendTextFromConsole() {
+        return false;
+    }
+
+    @Override
+    protected String getUserName() {
+        int x = (int) (Math.random() * 100);
+        return "date_bot_" + x;
+    }
+
+    public class BotSocketThread extends Client.SocketThread {
+    }
+}
