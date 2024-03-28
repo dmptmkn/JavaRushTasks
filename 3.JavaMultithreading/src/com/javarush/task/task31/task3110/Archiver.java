f4db7f9323a3c913f258a78487d4f96717ddee97
dmptmkn <dm.ptmkn@yandex.ru>
2024-03-28 10:49:55 +0300

    add solution to task multithreading3110

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
+public class Archiver {
+
+    public static void main(String[] args) {
+        String filePath = null;
+        String archivePath = null;
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Введите полный путь до архива:");
+            filePath = console.readLine();
+            System.out.println("Введите путь к файлу для архивации:");
+            archivePath = console.readLine();
+        } catch (Exception e) {
+        }
+
+        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(filePath));
+        try {
+            zipFileManager.createZip(Paths.get(archivePath));
+        } catch (Exception e) {
+        }
+    }
+
+}
