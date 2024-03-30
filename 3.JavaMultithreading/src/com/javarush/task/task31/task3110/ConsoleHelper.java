a37547888c32acf986105e01d58f6d5c9fa6c589
dmptmkn <dm.ptmkn@yandex.ru>
2024-03-30 15:16:25 +0300

    add solution to task multithreading3110.3

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ConsoleHelper.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleHelper {
+
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() throws IOException {
+        String s = null;
+        while (true) {
+            try {
+                s = reader.readLine();
+                break;
+            } catch (IOException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+        return s;
+    }
+
+    public static int readInt() throws IOException {
+        int i = 0;
+        while (true) {
+            try {
+                i = Integer.parseInt(reader.readLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+        return i;
+    }
+}
