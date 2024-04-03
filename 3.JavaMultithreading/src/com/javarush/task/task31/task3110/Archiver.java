1695537a1be1c1d5be8915e013286656e3987add
dmptmkn <dm.ptmkn@yandex.ru>
2024-04-03 11:12:37 +0300

    add solution to task multithreading3110

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,34 +1,57 @@
 package com.javarush.task.task31.task3110;
 
-import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.nio.file.Paths;
+import java.io.IOException;
 
 public class Archiver {
 
-    public static void main(String[] args) {
-        String filePath = null;
-        String archivePath = null;
-        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in))) {
-            System.out.println("Введите полный путь до архива:");
-            filePath = console.readLine();
-            System.out.println("Введите путь к файлу для архивации:");
-            archivePath = console.readLine();
-        } catch (Exception e) {
+    public static void main(String[] args) throws Exception {
+        Operation operation = null;
+        do {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
+        } while (operation != Operation.EXIT);
+    }
+
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Выберите операцию:\n" +
+                Operation.CREATE.ordinal() + " - упаковать файлы в архив\n" +
+                Operation.ADD.ordinal() + " - добавить файл в архив\n" +
+                Operation.REMOVE.ordinal() + " - удалить файл из архива\n" +
+                Operation.EXTRACT.ordinal() + " - распаковать архив\n" +
+                Operation.CONTENT.ordinal() + " - просмотреть содержимое архива\n" +
+                Operation.EXIT.ordinal() + " - выход");
+        int operationNum = ConsoleHelper.readInt();
+
+        Operation operation = null;
+        switch (operationNum) {
+            case 0:
+                operation = Operation.CREATE;
+                break;
+            case 1:
+                operation = Operation.ADD;
+                break;
+            case 2:
+                operation = Operation.REMOVE;
+                break;
+            case 3:
+                operation = Operation.EXTRACT;
+                break;
+            case 4:
+                operation = Operation.CONTENT;
+                break;
+            case 5:
+                operation = Operation.EXIT;
+                break;
         }
 
-        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(filePath));
-        try {
-            zipFileManager.createZip(Paths.get(archivePath));
-        } catch (Exception e) {
-        }
-
-        ExitCommand exit = new ExitCommand();
-        try {
-            exit.execute();
-        } catch (Exception e) {
-        }
+        return operation;
     }
 }
