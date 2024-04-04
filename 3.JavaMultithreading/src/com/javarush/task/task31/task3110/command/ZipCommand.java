1297d29f3b780d71619ee88b5f9243538278ef8e
dmptmkn <dm.ptmkn@yandex.ru>
2024-04-04 11:58:13 +0300

    add solution to task multithreading3110.11

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -1,5 +1,16 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+
+import java.nio.file.Paths;
+
 public abstract class ZipCommand implements Command {
+
+    public ZipFileManager getZipFileManager() throws Exception {
+        ConsoleHelper.writeMessage("Введите полный путь файла архива:");
+        String archivePath = ConsoleHelper.readString();
+        return new ZipFileManager(Paths.get(archivePath));
+    }
 
 }
