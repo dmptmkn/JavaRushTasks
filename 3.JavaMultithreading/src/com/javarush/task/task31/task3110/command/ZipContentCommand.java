4fb8e9258ab884c95bcd811ab5d78a48f3c89ed4
dmptmkn <dm.ptmkn@yandex.ru>
2024-04-04 13:24:27 +0300

    add solution to task multithreading3110.14

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
@@ -1,8 +1,16 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.FileProperties;
+import com.javarush.task.task31.task3110.ZipFileManager;
+
 public class ZipContentCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
-
+        ConsoleHelper.writeMessage("Просмотр содержимого архива.");
+        ConsoleHelper.writeMessage("Содержимое архива:");
+        getZipFileManager().getFilesList()
+                .forEach(fileProperties -> ConsoleHelper.writeMessage(fileProperties.toString()));
+        ConsoleHelper.writeMessage("Содержимое архива прочитано.");
     }
 }
