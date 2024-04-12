2da7e23a1ca1d5c11914bce7346afdf4380ce190
dmptmkn <dm.ptmkn@yandex.ru>
2024-04-12 17:49:56 +0300

    add solution to task multithreading3209.3

---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task32.task3209;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class View extends JFrame implements ActionListener {
+
+    private Controller controller;
+    private JTabbedPane tabbedPane = new JTabbedPane();
+    private JTextPane htmlTextPane = new JTextPane();
+    private JEditorPane plainTextPane = new JEditorPane();
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void init() {
+
+    }
+
+    public void exit() {
+        controller.exit();
+    }
+
+    public Controller getController() {
+        return controller;
+    }
+
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+}
