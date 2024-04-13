cfb7a0305a6a682083355787178844136bab7892
dmptmkn <dm.ptmkn@yandex.ru>
2024-04-13 23:56:59 +0300

    add solution to task multithreading3209

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -1,8 +1,10 @@
 package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
+import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -33,6 +35,14 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void initEditor() {
+        htmlTextPane.setContentType("text/html");
+        JScrollPane htmlPane = new JScrollPane(htmlTextPane);
+        tabbedPane.addTab("HTML", htmlPane);
+        JScrollPane textPane = new JScrollPane(plainTextPane);
+        tabbedPane.addTab("Текст", textPane);
+        tabbedPane.setPreferredSize(null);
+        tabbedPane.addChangeListener(new TabbedPaneChangeListener(this));
+        getContentPane().add(tabbedPane, BorderLayout.CENTER);
     }
 
     public void selectedTabChanged() {
