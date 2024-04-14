542b4dbfd5b62d3ead48b093242b4b821ad6a31f
dmptmkn <dm.ptmkn@yandex.ru>
2024-04-14 13:00:03 +0300

    add solution to task sql0216

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0216/query.sql
@@ -0,0 +1,7 @@
+SELECT CASE position
+           WHEN 'backend developer' THEN 'yes'
+           WHEN 'frontend developer' THEN 'yes'
+           ELSE 'no'
+           END
+FROM employee
+WHERE department = 'cool devs'
