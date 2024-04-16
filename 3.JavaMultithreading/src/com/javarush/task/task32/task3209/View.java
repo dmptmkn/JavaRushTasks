e00fe4bd9bdffe060760e127d2e268cdf9df2bbe
dmptmkn <dm.ptmkn@yandex.ru>
2024-04-16 08:33:33 +0300

    add solution to task multithreading3209.10

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -67,6 +67,14 @@ public class View extends JFrame implements ActionListener {
 
     }
 
+    public boolean canUndo() {
+        return false;
+    }
+
+    public boolean canRedo() {
+        return false;
+    }
+
     public void exit() {
         controller.exit();
     }
