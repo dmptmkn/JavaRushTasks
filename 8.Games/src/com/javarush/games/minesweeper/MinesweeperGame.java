1ea9e37ff226223274fa65debf54cf7f775fbe2f
dmptmkn <dm.ptmkn@yandex.ru>
2024-04-21 11:10:45 +0300

    add solution to task minesweeper5

---
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -0,0 +1,30 @@
+package com.javarush.games.minesweeper;
+
+import com.javarush.engine.cell.*;
+
+public class MinesweeperGame extends Game {
+
+    private static final int SIDE = 9;
+    private int countMinesOnField = 0;
+    private GameObject[][] gameField = new GameObject[SIDE][SIDE];
+
+    public void initialize() {
+        setScreenSize(SIDE, SIDE);
+        createGame();
+    }
+
+    private void createGame() {
+        for (int y = 0; y < SIDE; y++) {
+            for (int x = 0; x < SIDE; x++) {
+                if (getRandomNumber(10) == 0) {
+                    gameField[y][x] = new GameObject(x, y, true);
+                    countMinesOnField++;
+                } else {
+                    gameField[y][x] = new GameObject(x, y, false);
+                }
+                setCellColor(x, y, Color.ORANGE);
+            }
+        }
+    }
+
+}
