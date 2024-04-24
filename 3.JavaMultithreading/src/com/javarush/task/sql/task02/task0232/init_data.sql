52ea64174b048578817ed39fe3f2fc5f81b2f214
dmptmkn <dm.ptmkn@yandex.ru>
2024-04-24 22:20:52 +0300

    add solution to task sql0232

--- 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0232/init_data.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0232/init_data.sql
@@ -5,14 +5,14 @@ CREATE TABLE students
     `id`           INT          not null auto_increment primary key,
     `name`         VARCHAR(100) not null,
     `age`          INT          not null,
-    `is_full_time` INT          not null
+    `is_full_time` VARCHAR(100) not null
 );
 
 insert into students (id, name, age, is_full_time)
-values (11, 'Mary', 21, 0),
-       (2, 'Peter', 24, 1),
-       (5, 'Amy', 22, 0),
-       (7, 'John', 19, 1),
-       (6, 'Dexter', 40, 3),
-       (1, 'Maria', 26, 1),
-       (9, 'Alex', 21, 2);
+values (11, 'Mary', 21, 'no'),
+       (2, 'Peter', 24, 'yes'),
+       (5, 'Amy', 22, 'no'),
+       (7, 'John', 19, 'yes'),
+       (6, 'Dexter', 40, 'yes'),
+       (1, 'Maria', 26, 'yes'),
+       (9, 'Alex', 21, 'yes');
