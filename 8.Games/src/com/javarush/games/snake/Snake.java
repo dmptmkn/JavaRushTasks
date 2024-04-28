0ad2fd0cf3197f7e5303d2ca95d919d318092482
dmptmkn <dm.ptmkn@yandex.ru>
2024-04-28 07:46:08 +0300

    add solution to task snake6

--- 8.Games/src/com/javarush/games/snake/Snake.java
+++ 8.Games/src/com/javarush/games/snake/Snake.java
@@ -8,12 +8,24 @@ import java.util.List;
 
 public class Snake extends GameObject {
 
+    private static final String HEAD_SIGN = "\uD83D\uDC7E";
+    private static final String BODY_SIGN = "\u26AB";
+
+
     private List<GameObject> snakeParts = new ArrayList<>();
 
     public Snake(int x, int y) {
         super(x, y);
         for (int i = 0; i < 3; i++) {
             snakeParts.add(new GameObject(x + i, y));
+        }
+    }
+
+    public void draw(Game game) {
+        for (int i = 0; i < snakeParts.size(); i++) {
+            GameObject part = snakeParts.get(i);
+            String sign = (i != 0) ? BODY_SIGN : HEAD_SIGN;
+            game.setCellValue(part.x, part.y, sign);
         }
     }
 }
--- 8.Games/src/com/javarush/games/snake/SnakeGame.java
+++ 8.Games/src/com/javarush/games/snake/SnakeGame.java
@@ -4,6 +4,7 @@ import com.javarush.engine.cell.*;
 
 public class SnakeGame extends Game {
 
+    private Snake snake;
     public static final int WIDTH = 15;
     public static final int HEIGHT = 15;
 
@@ -19,10 +20,11 @@ public class SnakeGame extends Game {
                 setCellColor(x, y, Color.GRAY);
             }
         }
-
+        snake.draw(this);
     }
 
     private void createGame() {
+        this.snake = new Snake(WIDTH / 2, HEIGHT / 2);
         drawScene();
     }
 }
