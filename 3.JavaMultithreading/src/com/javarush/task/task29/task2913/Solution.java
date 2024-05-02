11e2607dbee895bc27fdaf5233dc6a969b931b3e
dmptmkn <dm.ptmkn@yandex.ru>
2024-05-02 13:32:14 +0300

    add solution to task multithreading2913

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2913/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task29.task2913;
+
+import java.util.Random;
+
+/* 
+Замена рекурсии
+*/
+
+public class Solution {
+    private static int numberA;
+    private static int numberB;
+
+    public static String getAllNumbersBetween(int a, int b) {
+        StringBuilder allNumbersBetween = new StringBuilder();
+        if (a < b) {
+            for (int i = a; i <= b; i++) {
+                allNumbersBetween.append(i);
+                allNumbersBetween.append(" ");
+            }
+        } else if (a > b) {
+            for (int i = a; i >= b; i--) {
+                allNumbersBetween.append(i);
+                allNumbersBetween.append(" ");
+            }
+        }
+
+        return allNumbersBetween.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        numberA = random.nextInt(1000);
+        numberB = random.nextInt(1000);
+        System.out.println(getAllNumbersBetween(numberA, numberB));
+        System.out.println(getAllNumbersBetween(numberB, numberA));
+    }
+}
