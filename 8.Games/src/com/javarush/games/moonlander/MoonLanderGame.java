300a9f6d6cff77517bc1f2276d35608e73c657cc
dmptmkn <dm.ptmkn@yandex.ru>
2024-05-02 23:51:19 +0300

    add solution to task moonlander2

--- 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
+++ 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
@@ -11,5 +11,18 @@ public class MoonLanderGame extends Game {
     @Override
     public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
+        createGame();
+    }
+
+    private void drawScene() {
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                setCellColor(x, y, Color.ORANGE);
+            }
+        }
+    }
+
+    private void createGame() {
+        drawScene();
     }
 }
