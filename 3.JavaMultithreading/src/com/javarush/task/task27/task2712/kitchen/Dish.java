cb616ce05a18b8511767ada7a43d5201fe0a5bbe
dmptmkn <dm.ptmkn@yandex.ru>
2024-05-03 09:31:51 +0300

    add solution to multithreading2712.1

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Dish.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task27.task2712.kitchen;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public enum Dish {
+    FISH,
+    STEAK,
+    SOUP,
+    JUICE,
+    WATER;
+
+    public static String allDishesToString() {
+        return Arrays.stream(values())
+                .map(Enum::toString)
+                .collect(Collectors.joining(", "));
+    }
+
+}
