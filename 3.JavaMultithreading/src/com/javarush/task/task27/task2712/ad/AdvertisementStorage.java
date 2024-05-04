f659348f417e64a1e4c44fedf31f540bf9e489bf
dmptmkn <dm.ptmkn@yandex.ru>
2024-05-04 12:04:30 +0300

    add solution to multithreading2712.6

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task27.task2712.ad;
+
+public class AdvertisementStorage {
+
+    private static AdvertisementStorage instance;
+
+    private AdvertisementStorage() {
+    }
+
+    public static AdvertisementStorage getInstance() {
+        if (instance == null) {
+            instance = new AdvertisementStorage();
+        }
+
+        return instance;
+    }
+}
