be3554f77b2894ec15af79adc50844731a19e273
dmptmkn <dm.ptmkn@yandex.ru>
2024-05-08 07:31:33 +0300

    add solution to multithreading3513.3

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,4 +1,18 @@
 package com.javarush.task.task35.task3513;
 
 public class Model {
+
+    private static final int FIELD_WIDTH = 4;
+
+    private Tile[][] gameTiles;
+
+    public Model() {
+        gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+
+        for (int y = 0; y < FIELD_WIDTH; y++) {
+            for (int x = 0; x < FIELD_WIDTH; x++) {
+                gameTiles[y][x] = new Tile();
+            }
+        }
+    }
 }
