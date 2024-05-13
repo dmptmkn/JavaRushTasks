f14bd38fbb8c3dd286c7e95a297f1c94e9f62eb9
dmptmkn <dm.ptmkn@yandex.ru>
2024-05-13 23:31:21 +0300

    add solution to sql0327

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0327/query.sql
@@ -0,0 +1,5 @@
+SELECT customers.city, orders.store_id
+FROM customers
+         RIGHT JOIN orders ON customers.customer_id = orders.customer_id
+WHERE orders.order_status = 'SHIPPED'
+  AND orders.total_cost > 100
