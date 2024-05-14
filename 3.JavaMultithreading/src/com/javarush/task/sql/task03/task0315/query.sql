f3c3ea0f4d14db15b4d4925ecb581da2e4f0b540
dmptmkn <dm.ptmkn@yandex.ru>
2024-05-14 16:26:48 +0300

    add updated solution to sql0315

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0315/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0315/query.sql
@@ -1,3 +1,3 @@
 SELECT ret.hq_location AS ret_location, sup.annual_revenue_billions AS sup_revenue
 FROM top_retailers AS ret, suppliers AS sup
-WHERE ret_location != 'USA'
+WHERE ret.hq_location != 'USA'
