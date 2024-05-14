9bca5fd0349fb45aa93b8fbf60e978e461987dd3
dmptmkn <dm.ptmkn@yandex.ru>
2024-05-14 16:24:36 +0300

    add solution to sql0335

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0335/query.sql
@@ -0,0 +1,6 @@
+SELECT author.country AS author_country, COUNT(book.book_id) AS book_count
+FROM authors AS author
+         JOIN books AS book ON author.author_id = book.author_id
+WHERE book.genre = 'novel'
+GROUP BY author.country
+HAVING book_count > 2
