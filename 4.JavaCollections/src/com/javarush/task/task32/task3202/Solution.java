815695221a93a22f1292fa68d91206f5a23ef736
dmptmkn <dm.ptmkn@yandex.ru>
2024-08-02 22:36:29 +0300

    add solution to task collections3202

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3202/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task32.task3202;
+
+import java.io.*;
+
+/* 
+Читаем из потока
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        StringWriter writer = getAllDataFromInputStream(new FileInputStream("testFile.log"));
+        System.out.println(writer.toString());
+    }
+
+    public static StringWriter getAllDataFromInputStream(InputStream is) throws IOException {
+        StringWriter writer = new StringWriter();
+        if (is == null) return writer;
+
+        try (InputStreamReader reader = new InputStreamReader(is)) {
+            while (reader.ready()) {
+                writer.write(reader.read());
+            }
+        }
+
+        return writer;
+    }
+}
